f38f7a20586612ef0ca4cf213ae5d4fd9553db2b
hkhalifa <hkhalifa@deloitte.de>
2025-09-09 10:23:51 +0200

    comments for understanding

--- config/config.py
+++ config/config.py
@@ -13,10 +13,10 @@ def get_config():
         'API_BASE_URL': os.getenv('API_BASE_URL'),
         # Optional filter
         'API_USER_ID': os.getenv('API_USER_ID'), 
-        'USE_S3': str_to_bool(os.getenv("USE_S3", "False")),
+        'USE_S3': str_to_bool(os.getenv("USE_S3", "False")), # if set to True in .env variables, upload to AWS
         'AWS_S3_BUCKET': os.getenv("AWS_S3_BUCKET"),
         'S3_PREFIX': os.getenv("S3_PREFIX"),
         'AWS_REGION': os.getenv("AWS_DEFAULT_REGION"),
         'OUTPUT_DIR': os.getenv("OUTPUT_DIR"),
-        'PARTITION_COLS': part_cols,
+        'PARTITION_COLS': part_cols, # can be comma sperated in .env file
     }
